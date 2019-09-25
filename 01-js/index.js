8b8ada13f828b24bdca9d026df0b26ba463dd7ac
LA-BESTIA--PABL\pjord <pjorda96@gmail.com>
2019-09-25 21:58:44 +0200

    actividad 1 empezada

---
+++ 01-js/index.js
@@ -0,0 +1,63 @@
+const rl = require('readline-sync');
+
+let salir = false;
+
+const calculadorTarifa = () => {
+  let option = parseInt(rl.question('Introduce los minutos: '));
+  let precio = 10;
+  const bp1 = 180, bp2 = 240;
+  const dif = bp2 - bp1;
+
+  if (option > bp1) {
+    option -= bp1;
+
+    if (option > dif) {
+      const dif = bp2 - bp1;
+      precio += .10 * dif;
+      option -= dif;
+
+      precio += .20 * option;
+    } else {
+      precio += .10 * option;
+    }
+  }
+
+  console.log(`\n La cantidad a pagar es ${precio}€ \n \n`);
+};
+
+/*Crea un programa que permita generar su lista de la compra.
+  El programa solicitará al usuario ítems a añadir a la lista de la compra.
+  El programa ignorará los ítems que ya han sido añadidos y no los añadirá de nuevo.
+  Cuando el usuario finalice de introducir ítems, el programa mostrará toda la lista de
+ítems que el usuario ha introducido.*/
+
+const listaCompra = () => {
+  let option = parseInt(rl.question('Introduce los minutos: '));
+
+};
+
+while(!salir){
+  console.log('Elige una opcion de la lista o -1 para salir');
+  console.log('1: Calculador de tarifa');
+  let option = parseInt(rl.question('Introduce una opcion: '));
+
+  switch (option) {
+    case 1:
+      calculadorTarifa();
+      break;
+
+    case 2:
+      listaCompra();
+      break;
+
+    case -1:
+      salir = true;
+      break;
+
+
+    default:
+      console.log('Algo ha ido mal, elige una opcion de la lista o -1 para salir');
+      break;
+
+  }
+}
