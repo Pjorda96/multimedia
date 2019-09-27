c1175d8b82bd6f1a5caae4281d1de594035a1c6b
LA-BESTIA--PABL\pjord <pjorda96@gmail.com>
2019-09-27 20:52:13 +0200

    ejercicios 12345

--- 01-js/index.js
+++ 01-js/index.js
@@ -1,8 +1,10 @@
-'use strict'
+'use strict';
 
 const rl = require('readline-sync');
 
 let salir = false;
+let dataArray = [];
+let fin = false;
 
 const calculadorTarifa = () => {
   let option = parseInt(rl.question('Introduce los minutos: '));
@@ -24,14 +26,9 @@ const calculadorTarifa = () => {
     }
   }
 
-  console.log(`\n La cantidad a pagar es ${precio}€ \n \n`);
+  console.log(`\nLa cantidad a pagar es ${precio}€\n`);
 };
 
-/*Crea un programa que permita generar su lista de la compra.
-  El programa solicitará al usuario ítems a añadir a la lista de la compra.
-  El programa ignorará los ítems que ya han sido añadidos y no los añadirá de nuevo.
-  Cuando el usuario finalice de introducir ítems, el programa mostrará toda la lista de
-ítems que el usuario ha introducido.*/
 
 const listaCompra = () => {
   let lista = [];
@@ -47,14 +44,61 @@ const listaCompra = () => {
     }
   }
 
+  console.log('\nCosas a comprar: ');
   lista.map(articulo => console.log(`- ${articulo}`));
+};
 
+
+const standardDeviation = dataArray => {
+  const calcularMedia = dataArray => {
+    let suma = 0;
+
+    dataArray.map(x => suma += x);
+
+    return suma/dataArray.length;
+  };
+
+  let resultado = 0;
+  const media = calcularMedia(dataArray);
+
+  dataArray.map(x => {
+    const res = x - media;
+    resultado += res * res;
+  });
+
+  resultado = Math.sqrt(resultado/dataArray.length);
+  console.log(`\nLa desviacion tipica es ${resultado}\n`);
+};
+
+
+const withoutVowels  = text => {
+  let result = '';
+
+  for (let i = 0; i < text.length; i++) {
+    const char = text.charAt(i);
+
+    char !== 'a' && char !== 'e' && char !== 'i' && char !== 'o' && char !== 'u' && (result += char);
+  }
+
+  console.log(`\n${result}\n`);
+};
+
+
+const linearSearch = (number, dataArray) => {
+  let exists = false;
+
+  dataArray.map(x => x === number && (exists = true));
+
+  exists ? console.log('\nNumero encontrado\n') : console.log('\nNumero no encontrado\n')
 };
 
 while(!salir){
   console.log('Elige una opcion de la lista o -1 para salir');
   console.log('1: Calculador de tarifa');
   console.log('2: Lista de la compra');
+  console.log('3: Calcular desviacion tipica');
+  console.log('4: Eliminar vocales');
+  console.log('5: Buscador de numero');
   let option = parseInt(rl.question('Introduce una opcion: '));
 
   switch (option) {
@@ -64,6 +108,46 @@ while(!salir){
 
     case 2:
       listaCompra();
+      break;
+
+    case 3:
+      dataArray = [];
+      fin = false;
+
+      while (!fin) {
+        let option = parseInt(rl.question('Introduce un numero o -1 para calcular: '));
+
+        if (option !== -1) {
+          dataArray.push(option);
+        } else {
+          fin = !fin;
+        }
+      }
+
+      standardDeviation(dataArray);
+      break;
+
+    case 4:
+      withoutVowels(rl.question('Introduce un texto: \n'))
+      break;
+
+    case 5:
+      dataArray = [];
+      fin = false;
+
+      const number = parseInt(rl.question('Introduce el numero a buscar: '));
+
+      while (!fin) {
+        let option = parseInt(rl.question('Introduce un numero o -1 para calcular: '));
+
+        if (option !== -1) {
+          dataArray.push(option);
+        } else {
+          fin = !fin;
+        }
+      }
+
+      linearSearch(number, dataArray);
       break;
 
     case -1:
